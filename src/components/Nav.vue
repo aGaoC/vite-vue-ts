cd6e863603b1300a5b1bdde124f4d4199751b5d7
aGaoC <46312464+aGaoC@users.noreply.github.com>
2021-04-27 16:03:27 +0800

    feat(components): 新增登陆页

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -28,7 +28,7 @@ export default defineComponent({
         {
           name: 'Home',
           isActive: false,
-          path: '/'
+          path: '/home'
         },
         {
           name: 'Vuex',
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,43 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '@/view/home.vue'
 import Vuex from '@/view/vuex.vue'
+import login from '@/view/login/login.vue'
+import Main from '@/components/main.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/login',
+    name: 'login',
+    component: login
+  },
+  {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/vuex',
-    name: 'Vuex',
-    component: Vuex
-  },
-  {
-    path: '/axios',
-    name: 'Axios',
-    component: () => import('@/view/axios.vue') // 懒加载组件
+    name: 'index',
+    component: Main,
+    redirect: 'home',
+    children: [
+      {
+        path: '/home',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/vuex',
+        name: 'Vuex',
+        component: Vuex
+      },
+      {
+        path: '/axios',
+        name: 'Axios',
+        component: () => import('@/view/axios.vue') // 懒加载组件
+      }
+    ]
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
