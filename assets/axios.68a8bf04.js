88b2b5f803bccae227a41bb19ed954d9bce14a9f
aGaoC <aGaoC@users.noreply.github.com>
2021-04-27 05:27:08 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 7b1502670fca554563192b452bec8abc2c17358c

--- assets/axios.68a8bf04.js
+++
@@ -1,1 +0,0 @@
-import{o as r,c as e}from"./vendor.b8ec1473.js";const o={data(){}};o.render=function(o,t,n,a,d,c){return r(),e("div")};export default o;
---
+++ assets/axios.a8661a2c.js
@@ -0,0 +1,1 @@
+import{o as r,c as o}from"./vendor.12c2f92f.js";const t={data(){}};t.render=function(t,e,n,a,d,f){return r(),o("div")};export default t;
