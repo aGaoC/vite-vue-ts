007dd019e84437bdf09c06ca5585a618c4fa57fb
aGaoC <46312464+aGaoC@users.noreply.github.com>
2021-04-27 11:38:21 +0800

    feat(components): 新增首页模块

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Main />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 })
 </script>
