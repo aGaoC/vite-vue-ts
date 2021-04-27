420356c80c8acfd6d54578f75cde25e436f20a43
aGaoC <46312464+aGaoC@users.noreply.github.com>
2021-04-27 13:22:43 +0800

    fix(components): 1

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Main from '@/components/Main.vue'
+import Main from './components/Main.vue'
 
 export default defineComponent({
   name: 'App',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
 import router from './router/index'
-import store from './store/index'
-import ElementPlus from 'element-plus'
+// import store from './store/index'
 import 'element-plus/lib/theme-chalk/index.css'
+
 createApp(App).use(router).mount('#app')
